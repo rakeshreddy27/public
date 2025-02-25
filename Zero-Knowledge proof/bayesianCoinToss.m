a18871e18213119b0ab7fe862d3fa92b2d925bef
ZBOOK\User <>
2025-02-24 20:53:15 -0500

    bayesian method

--- Zero-Knowledge proof/bayesianCoinToss.m
+++ Zero-Knowledge proof/bayesianCoinToss.m
@@ -2,6 +2,6 @@
 % iteration and number of bins to plot the histogram
 
 
+bayesianCoinTossBasic(10000, 1000, 100, 'normal', 0.5, 0);
 %bayesianCoinTossBasic(10000, 1000, 100, 'uniform');
-bayesianCoinTossBasic(10000, 1000, 100, 'normal');
 
--- Zero-Knowledge proof/bayesianCoinTossBasic.m
+++ Zero-Knowledge proof/bayesianCoinTossBasic.m
@@ -1,12 +1,20 @@
-function bayesianCoinTossBasic = bayesianCointTossBasic(iterations, tossesPerIteration, bins, typeOfDistributionOfRand)
+function bayesianCoinTossBasic = bayesianCointTossBasic(iterations, tossesPerIteration, bins, typeOfDistributionOfRand, mu, sigma)
 % iterations how many simulated runs? Here I used 10000 total simulated runs.
-ph = 0.5; % the coin is fair. ph is the probability of a head/tail
+
+p = 0.5; % the coin is fair. ph is the probability of a head/tail
+
+% if the coin toss follows normal distribution, mean of each batch will be
+% close to the mean
+% adust 
 if strcmp(typeOfDistributionOfRand, 'normal')
-    tosses = (normrnd(0.5, 0.2, iterations, tossesPerIteration) <= ph)*2 - 1;
-else if strcmp(typeOfDistributionOfRand, 'uniform')
-        tosses =  (rand(iterations, tossesPerIteration) <= ph)*2 - 1;
+    tosses = (normrnd(mu, sigma, iterations, tossesPerIteration) <= p)*2 - 1;
 end
 
-meanToss = mean(tosses, 1);
+% Follows uniform distribution, i.e all the values are equally likely
+if strcmp(typeOfDistributionOfRand, 'uniform')
+        tosses =  (rand(iterations, tossesPerIteration) <= p)*2 - 1;
+end
+
+meanToss = mean(tosses, 2);
 histfit(meanToss,bins);
 end
